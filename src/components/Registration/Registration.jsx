0ce195229196fc5a374c50ca318ad160d9fb968d
IbragimovAbduaziz <abduazizibragimov74@gmail.com>
2024-02-27 22:25:52 +0500

    jhkjhkjhkjhkjh

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -5,13 +5,13 @@ export default function Registration() {
    <section id="sing-in">
      <div className='container flex justify-center align-center'>
       <div className="form-group flex direction-column">
-          <h2 className='pt-sans-bold'>Korxonani ro'yxatdan o'tkazish</h2>
+          <h2 className='pt-sans-bold'>Korxonani qo'shish.</h2>
           <form>
               <div className="item-form">
                 <input type="text" name="compony" placeholder='Korxonani nomi..'/>
                 </div>
               <div className="item-form">
-                <input type="number" name="stir" placeholder='INN kiriting...'/>
+                <input type="number" name="stir" placeholder='STIR kiriting...'/>
                 </div>
               <div className="item-form">
                 <input type="submit" value="Submit"/>
