8d3bfa9bb5cd4613477650e148205c07a1b0c48f
IbragimovAbduaziz <abduazizibragimov74@gmail.com>
2024-02-27 22:35:07 +0500

    HJGJHGJHG

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './css/style.css';
 import Registration from './components/Registration/Registration';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Registration/>
+     <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/register" element={<Registration />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
