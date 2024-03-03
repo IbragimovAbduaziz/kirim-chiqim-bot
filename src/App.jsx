b3ed3e62b83a533992b859889b84a5e2ff44824b
IbragimovAbduaziz <abduazizibragimov74@gmail.com>
2024-03-03 20:47:09 +0500

    a1

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import './css/style.css';
 import Registration from './components/Register/Register';
 import Ombor from './components/Ombor/Ombor';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from 'react';
+
+const tg=window.Telegram.WebApp
 
 function App() {
+  const [warehouse,setWarehouse]=useState("")
+  useEffect(()=>{
+      tg.ready()
+  })
+  const handleWare=(e)=>{
+    setWarehouse(e.target.value)
+  }
+  const sendWare=()=>{
+    tg.mainButton.text="Qoshish :)"
+    tg.mainButton.show()
+  }
   return (
     <>
      <BrowserRouter>
       <Routes>
-          <Route path="register" element={<Registration />} />
+          <Route path="register" element={<Registration warehouse={warehouse} handleWare={handleWare} sendWare={sendWare} />} />
           <Route path="ombor" element={<Ombor />} />
       </Routes>
     </BrowserRouter>
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,23 +1,6 @@
-import { useCallback, useEffect, useState } from 'react';
 import house from '../../../public/house.png';
-const telegram=window.Telegram.WebApp
 
-export default function Register() {
-  const [name,setName]=useState("")
-  useEffect(()=>{
-    telegram.ready();
-  })
-    
-  const handleName=(e)=>{
-    setName(e.target.value)
-    telegram.MainButton.text="Qo'shish.";
-    telegram.MainButton.show()
-  }
-
-  useEffect(()=>{
-    telegram.onEvent('mainButtonClicked',()=>telegram.sendData(JSON.stringify(name)));
-    return ()=>telegram.offEvent('mainButtonClicked',()=>telegram.sendData(JSON.stringify(name)));
-  })
+export default function Register({warehouse,handleWare,sendWare}) {
   return(
     <section id="register">
       <div className="container">
@@ -25,8 +8,8 @@ export default function Register() {
             <div className="form-group">
               <img src={house}/>
               <h3>Ombor yoki do'kon qo'shish!</h3>
-              <input type="text" placeholder="Ombor nomini qo'shing" onChange={handleName}/>
-              <input type="button" value="Qo'shish"/>
+              <input type="text" placeholder="Ombor nomini qo'shing" onChange={handleWare}/>
+              <input type="button" value="Qo'shish" onClick={sendWare}/>
             </div>
         </div>
       </div>
