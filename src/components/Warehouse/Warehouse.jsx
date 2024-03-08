4aca8211f79846a2346788afa734ba5db013ec43
IbragimovAbduaziz <abduazizibragimov74@gmail.com>
2024-03-08 11:16:51 +0500

    w4

--- src/components/Warehouse/Warehouse.jsx
+++ src/components/Warehouse/Warehouse.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { useSearchParams } from "react-router-dom";
 export default function Warehouse() {
+  let [searchParams, setSearchParams] = useSearchParams();
+  let [wares,setWares]=useState("")
+  function handleSubmit(event) {
+    event.preventDefault();
+    let params = serializeFormQuery(event.target);
+    setSearchParams(params);
+    useEffect(()=>{
+      axios.get(`https://kirim-chiqim-ombor.uz/${searchParams}`)
+      .then((data=>{
+          setWares(JSON.stringify(date))
+      }))
+      .catch(err=>{
+        console.log(err);
+      })
+    })
+  }
   return (
     <section id="warehouse">
       <div className="container">
