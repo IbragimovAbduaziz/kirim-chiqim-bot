62c18510cd917e1734101f7ce0b9aba64bbb4519
IbragimovAbduaziz <abduazizibragimov74@gmail.com>
2024-03-09 11:20:41 +0500

    r3

--- src/components/Warehouse/Views.jsx
+++ src/components/Warehouse/Views.jsx
@@ -4,13 +4,22 @@ import { useSearchParams} from "react-router-dom";
 
 export default function Views() {
   let [searchParams, setSearchParams] = useSearchParams();
+  let [ware,setWare]=useState()
   const id = searchParams.get('id')
   const wareId=searchParams.get('ware')
+  useEffect(()=>{
+    axios.get(`https://kirim-chiqim-ombor.uz/ombor/${wareId}`)
+    .then(data=>{
+      setWare(data.data)
+    })
+    .catch(err=>{
+      console.log("xatooo"+err);
+    })
+  },[wareId])
   return (
     <section id="view">
         <div className="container">
-            <h2>{id}</h2>
-            <h3>{wareId}</h3>
+            {JSON.stringify(ware)}
         </div>
     </section>
   )
